f58c1679f547a0ce025960fa5832a394f989528c
M Groesbeck <m.groesbeck@gmail.com>
2020-04-10 10:35:11 -0700

    Made progress on client React app; currently running into CORS problems. This should not be an issue for a deployed server. Reading CORS documentation.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 
 const projectRouter = require("./data/routers/projectRouter.js");
 const actionRouter = require("./data/routers/actionRouter.js");
@@ -6,6 +7,7 @@ const actionRouter = require("./data/routers/actionRouter.js");
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 // middleware
 
